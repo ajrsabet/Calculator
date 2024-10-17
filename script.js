27d5c9f5b39ffb8b1c58cb3ebe0b0a40516776e3
Adam Sabet <arishel@gmail.com>
2024-10-16 19:17:19 -0700

    Create basic js and html

---
+++ script.js
@@ -0,0 +1,34 @@
+// 3. Calculator
+// Create a program that functions as a calculator. 
+// The program stores each user input one at a time until an "=" sign is input. 
+// At which point the values input should be processed as a calculator would. 
+// 1000 + 5 * 3 =
+// Assume this is input one at a time. [1,0,0,0,"+",5,"*",3,"="] 
+
+// Order of operations must be considered. Parenthesis and Exponents need not be handled nor do you have to handle decimals. 
+// Major Extra credit if you handle the PE in PEMDAS and add decimals.
+
+// Values it must handle. 
+// [0,1,2,3,4,5,6,7,8,9,"+","-","/","*","="]
+
+// event controller
+
+// addition
+function addition(params) {
+    
+}
+
+// subtraction
+
+// division
+
+// multiplication
+
+// event listener for submit button
+        // Select the button element
+        const button = document.getElementById('equationButton');
+
+        // Add an event listener to the button for a 'click' event
+        button.addEventListener('click', function() {
+            alert('Button was clicked!');
+        });
